e08a2463366c6f0ffa46388516e55d12d84c10f5
dwLG00 <wallace.dylan00@gmail.com>
2024-05-25 23:59:40 -0700

    Added compression effect

--- src/effects.rs
+++ src/effects.rs
@@ -1,3 +1,18 @@
 use crate::wave;
 
+pub fn compressor(audio: Vec<f64>, cutoff: f64, ratio: f64) -> Vec<f64> {
+    // Compressor effect
 
+    let mut buffer = Vec::<f64>::new();
+    for sample in audio {
+        if sample.abs() < cutoff {
+            buffer.push(sample);
+        } else {
+            let polarity: f64 = if sample > 0.0 { 1.0 } else { -1.0 };
+            let amount_above = sample.abs() - cutoff;
+            let compressed_amount = polarity * (amount_above / ratio + cutoff);
+            buffer.push(compressed_amount);
+        }
+    }
+    buffer
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod wave;
+mod effects;
 
 fn main() {
 }
